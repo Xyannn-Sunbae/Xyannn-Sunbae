c0937bf900aaf22a7639e5eac8126d7a38f156d9
Xyannn-Sunbae <xyannnid@gmail.com>
2025-02-01 19:49:48 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,23 @@
+const nama = "BRIANNNN";
+let usia = 18;
+
+function generalBiodata() {
+    if (usia == 18) {
+        //KONDISI PERTAMA
+        console.log('umur kamu telah mencukupi');
+        // KONDISI PERTAMA
+    } else {
+        //KONDISI TIDAK TERPENUHI
+        console.log('kamu belum cukup umur');
+    }
+
+  
+}
+
+console.log(nama);
+console.log(usia);
+
+generalBiodata();
+
+
+
